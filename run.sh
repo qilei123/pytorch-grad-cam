756bdc4a2c85c6b057d2b3049b1016b130e6da1b
qilei123 <qileimail123@gmail.com>
2022-05-07 02:53:53 -0400

    test cam

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #python usage_examples/swinT_example_test.py \
 python cam_test.py \
     --use-cuda \
-    --method resnext50_32x4d \
+    --method gradcam \
     --image-path /data3/qilei_chen/DATA/polyp_xinzi/D2/preprocessed/test/none_adenoma/ \
     --pth_dir /data3/qilei_chen/DATA/polyp_xinzi/preprocessed_4_classification/work_dir/resnext50_32x4d-224/model_best.pth.tar \
     --save_dir /data3/qilei_chen/DATA/polyp_xinzi/D2/preprocessed/cam/resnext50_32x4d/none_adenoma
