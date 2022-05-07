66f5acb31eec12623a338ea95491b030a3d8e82c
qilei123 <qileimail123@gmail.com>
2022-05-07 01:42:53 -0400

    test cam

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 python usage_examples/swinT_example_test.py \
     --use-cuda \
-    --image-path /data3/qilei_chen/DATA/polyp_xinzi/D2/preprocessed/test/adenoma/1bbcc278-f3a0-4f1e-a00a-3863fe25b6aa.jpg
+    --image-path /data3/qilei_chen/DATA/polyp_xinzi/D2/preprocessed/test/adenoma/1bbcc278-f3a0-4f1e-a00a-3863fe25b6aa.jpg \
     --pth_dir /data3/qilei_chen/DATA/polyp_xinzi/preprocessed_4_classification/work_dir/swin_base_patch4_window7_224-224/model_best.pth.tar 
