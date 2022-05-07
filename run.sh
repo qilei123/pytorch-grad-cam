dad36562c556f3e3a873d894f3de659adcfa93c0
qilei123 <qileimail123@gmail.com>
2022-05-07 02:22:49 -0400

    test cam

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 python usage_examples/swinT_example_test.py \
     --use-cuda \
     --method gradcam \
-    --image-path /data3/qilei_chen/DATA/polyp_xinzi/D2/preprocessed/test/adenoma/ \
+    --image-path /data3/qilei_chen/DATA/polyp_xinzi/D2/preprocessed/test/none_adenoma/ \
     --pth_dir /data3/qilei_chen/DATA/polyp_xinzi/preprocessed_4_classification/work_dir/swin_base_patch4_window7_224-224/model_best.pth.tar \
-    --save_dir /data3/qilei_chen/DATA/polyp_xinzi/D2/preprocessed/cam/adenoma
+    --save_dir /data3/qilei_chen/DATA/polyp_xinzi/D2/preprocessed/cam/none_adenoma
