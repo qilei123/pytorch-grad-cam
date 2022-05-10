9afb1a585fb5f0b940bc8465d04bb7cdd2a00a65
qilei123 <qileimail123@gmail.com>
2022-05-10 12:39:16 -0400

    test

--- usage_examples/swinT_example_test.py
+++ usage_examples/swinT_example_test.py
@@ -235,7 +235,7 @@ def main2():
         orig_img = cv2.imread(img_path, 1)[:, :, ::-1]
         print(orig_img.shape)
         cam_image = cv2.resize(cam_image,orig_img.shape[:2])
-        print(orig_img.shape)
+        print(cam_image.shape)
         #im_h = cv2.vconcat([rgb_img, cam_image])
         im_h = np.concatenate((orig_img, cam_image), axis=1)
         cv2.imwrite(os.path.join(args.save_dir,os.path.basename(img_path)), im_h)
